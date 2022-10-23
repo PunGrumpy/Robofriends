1db97015d3838ae5f485ccb42befe9d09221ff63
PunGrumpy <noppakorn.pun@gmail.com>
2022-10-23 23:26:16 +0700

    react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,6 +16,7 @@ function App() {
 
     const [robots, setRobots] = useState([]);
     const [searchField, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
 
     // componentDidMount() {
     //     fetch('https://jsonplaceholder.typicode.com/users')
@@ -27,7 +28,9 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => setRobots(users));
-    });
+
+        console.log(count);
+    }, [count]); // only run if count changes
 
     const onSearchChange = event => {
         setSearchField(event.target.value);
@@ -46,6 +49,7 @@ function App() {
     ) : (
         <div className="tc">
             <h1 className="f1">RoboFriends</h1>
+            <button onClick={() => setCount(count + 1)}>Click Me</button>
             <SearchBox searchChange={onSearchChange} />
             <Scroll>
                 <ErrorBoundary>
